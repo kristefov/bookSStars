11807a902579351c1a2929dc273e6c662c013602
kristiyan <kristiyan.tefov@icloud.com>
2023-06-30 20:05:17 +0100

    importing apollo client in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,24 @@ import {
   createHttpLink,
 } from '@apollo/client';
 
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
 
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
 
 function App() {
   return (
